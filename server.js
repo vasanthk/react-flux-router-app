23fa206d235d9c03997c60cfbe9da96bf66ea587
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-06-19 14:00:37 -0700

    Update app configs

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+/* eslint-disable no-var, strict */
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true
-}).listen(5000, 'localhost', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
+  hot: true,
+  historyApiFallback: true
+});
 
-    console.log('Listening at localhost:5000');
-  });
+server.listen(5000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:5000');
+});
